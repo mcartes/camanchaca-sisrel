6716000a802fd872b7ca89f58b3558c70101d090
efuentealbaCH <efuentealba.git@gmail.com>
2024-02-01 00:14:50 -0300

    feat:(dashboard) se cuentan de manera mas presisa las iniciativas y organizaciones

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,15 +32,16 @@ class HomeController extends Controller
         $filtro_region = $request->region;
         $filtro_division = $request->division;
         // return $filtro_region;
-        $cantidadIniciativas = Iniciativas::where('inic_vigente', 'S')->join('iniciativas_unidades', 'iniciativas_unidades.inic_codigo', 'iniciativas.inic_codigo')
+        $cantidadIniciativas = Iniciativas::where('inic_vigente', 'S')
+            ->join('iniciativas_unidades', 'iniciativas_unidades.inic_codigo', 'iniciativas.inic_codigo')
             ->join('unidades', 'unidades.unid_codigo', 'iniciativas_unidades.unid_codigo')
             ->join('comunas', 'comunas.comu_codigo', 'unidades.comu_codigo')
             ->join('divisiones', 'divisiones.divi_codigo', 'unidades.divi_codigo')
             ->join('regiones', 'regiones.regi_codigo', 'comunas.regi_codigo');
-        $cantidadOrganizaciones = Organizaciones::where('orga_vigente', 'S')->join('comunas', 'comunas.comu_codigo', 'organizaciones.comu_codigo')
-            ->join('unidades', 'unidades.comu_codigo', 'comunas.comu_codigo')
-            ->join('divisiones', 'divisiones.divi_codigo', 'unidades.divi_codigo')
-            ->join('regiones', 'regiones.regi_codigo', 'comunas.regi_codigo');
+        $cantidadOrganizaciones = Organizaciones::where('orga_vigente', 'S')->leftjoin('comunas', 'comunas.comu_codigo', 'organizaciones.comu_codigo')
+            ->leftjoin('unidades', 'unidades.comu_codigo', 'comunas.comu_codigo')
+            ->leftjoin('divisiones', 'divisiones.divi_codigo', 'unidades.divi_codigo')
+            ->leftjoin('regiones', 'regiones.regi_codigo', 'comunas.regi_codigo');
         $costosDinero = CostosDinero::select(DB::raw('IFNULL(sum(codi_valorizacion), 0) as total'))->where('codi_vigente', 'S')
             ->join('iniciativas', 'iniciativas.inic_codigo', 'costos_dinero.inic_codigo')
             ->join('iniciativas_unidades', 'iniciativas_unidades.inic_codigo', 'iniciativas.inic_codigo')
@@ -113,8 +114,8 @@ class HomeController extends Controller
             $inviIniciativas->where('divisiones.divi_codigo', $filtro_division);
         }
 
-        $cantidadIniciativas = $cantidadIniciativas->count();
-        $cantidadOrganizaciones = $cantidadOrganizaciones->count();
+        $cantidadIniciativas = $cantidadIniciativas->count(DB::raw('DISTINCT iniciativas.inic_codigo'));
+        $cantidadOrganizaciones = $cantidadOrganizaciones->count(DB::raw('DISTINCT organizaciones.orga_codigo'));
         $costosDinero = $costosDinero->first()->total;
         $costosEspecies = $costosEspecies->first()->total;
         $costosInfra = $costosInfra->first()->total;
